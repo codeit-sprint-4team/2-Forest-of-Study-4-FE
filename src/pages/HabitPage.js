a499359ed4b1222e70398d690d487f90d2ab0744
alscksdlek <mseon916@naver.com>
2024-09-30 18:49:32 +0900

    feat: 현재 시간 추가

--- src/pages/HabitPage.js
+++ src/pages/HabitPage.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import Header from "../components/commons/header/Header";
+import "../style/habit.css";
 
 function HabitPage() {
+  const [todayTime, setTodayTime] = useState("");
+
+  //현재 시간 표시
+  useEffect(() => {
+    const updateTime = () => {
+      const todayTime = new Date();
+      const minuteSecond = todayTime.toLocaleTimeString("ko-KR", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+      const formattedDate = `${todayTime.getFullYear()}-${todayTime.getMonth()}-${todayTime.getDate()} ${minuteSecond}`;
+      setTodayTime(formattedDate);
+    };
+    updateTime();
+    const intervalId = setInterval(updateTime, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="habit">
-      <div className="habitOutside">
-        <div>
-          <Header
-            title="title"
-            buttonTo1="/"
-            buttonTo2="/"
-            buttonTitle1="오늘의 집중"
-            buttonTitle2="홈"
-          />
+    <Fragment>
+      <div className="habit">
+        <div className="habitOutside">
+          <div>
+            <Header
+              title="title"
+              buttonTo1="/"
+              buttonTo2="/"
+              buttonTitle1="오늘의 집중"
+              buttonTitle2="홈"
+            />
+          </div>
+          <div>
+            <p className="timeTitle">현재 시간</p>
+            <div className="timeDisplay">{todayTime}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
