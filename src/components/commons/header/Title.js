95912f8f012ea23f3e9ca83d8d88033253ccf31e
alscksdlek <mseon916@naver.com>
2024-09-30 18:30:46 +0900

    refactor: header title 코드 수정

--- src/components/commons/header/Title.js
+++ src/components/commons/header/Title.js
@@ -1,3 +1,12 @@
-export default function HeaderTitle({ title }) {
-  return <h1 className="headerTitle">{title}</h1>;
+import React from "react";
+
+function HeaderTitle({ title }) {
+  return (
+    <div className="headerTitle">
+      {title}
+      <span>의 개발공장</span>
+    </div>
+  );
 }
+
+export default HeaderTitle;
