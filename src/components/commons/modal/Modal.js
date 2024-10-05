d57501c4e9a2f4050fd498fb3b57a6f87a98246a
mingmungXD <rex02033@gmail.com>
2024-10-06 01:55:26 +0900

    모달 페이지 수정

--- src/components/commons/modal/Modal.js
+++ src/components/commons/modal/Modal.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import '../../../style/Modal.css'
+import { Link } from "react-router-dom";
+import btn_habit from "../../../assets/imgs/btn_habit.png"
+import btn_focus from "../../../assets/imgs/btn_focus.png"
 
 function Modal({ title, exittext, content, buttontext, handleButtonClick}) {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,6 +24,12 @@ function Modal({ title, exittext, content, buttontext, handleButtonClick}) {
             <div>{content}</div>
             <button className="submit-button" onClick={handleButtonClick}>{buttontext}</button>
           </div>
+          <Link to="/Habit"> 
+            <img src={btn_habit} />
+          </Link>
+          <Link to="/Focus">
+            <img src={btn_focus} />
+          </Link>
         </div>
       )}
     </div>
