f6afd8b9cfdd60ffa7f4eabd99c2dcf94c286d30
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2024-10-06 00:59:42 +0900

    feat: gnb 추가 (#21)
    * feat: gnb 추가
    
    * style: sticky 삭제
    
    ---------
    
    Co-authored-by: alscksdlek <mseon916@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HabitPage from "./pages/HabitPage";
+import TestPage from "./pages/testPage.js";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/habit" element={<HabitPage />} />
+        <Route path="/" element={<TestPage />} />
       </Routes>
     </Router>
   );
