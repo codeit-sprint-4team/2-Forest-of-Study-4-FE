fdf4963f35ab460938a19b2b83956b74015de83d
mingmungXD <rex02033@gmail.com>
2024-10-05 22:38:54 +0900

    feat: 공통 모달 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Test from "./pages/test";
+import Modal from "./components/commons/modal/Modal";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Test />} />
+        <Route path="/modal" element={<Modal
+  title="비밀번호"
+  exittext="나가기"
+  content="모달 내용입니다."
+  buttontext="수정하러 가기"
+  handleButtonClick="aa"
+/>} />
       </Routes>
     </Router>
   );
--- src/components/commons/modal/Modal.js
+++ src/components/commons/modal/Modal.js
@@ -1,64 +1,25 @@
 import React, { useState } from "react";
-import './Modal.css';
+import '../../../style/Modal.css'
 
-function Modal() {
+function Modal({ title, exittext, content, buttontext, handleButtonClick}) {
   const [isOpen, setIsOpen] = useState(false);
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); 
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => {
     setIsOpen(false);
-    setErrorMessage("");
   };
-
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch("어떤 주소가 있겠지요 아직은 없지만...", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ password }),
-      });
-
-      const result = await response.json();
-
-      if (response.ok) {
-        alert("비밀번호가 맞습니다!");
-        closeModal();
-      } else {
-        setErrorMessage(result.message || "비밀번호가 틀렸습니다.");
-      }
-    } catch (error) {
-      console.error("에러 발생:", error);
-      setErrorMessage("오류가 발생했습니다.");
-    }
-  };
-
   return (
     <div>
-      <button onClick={openModal}>모달을 여는 무언가</button>
+      <button onClick={openModal}>모달을 여는 버튼입니다</button>
       {isOpen && (
         <div className="modal">
-          <div className="modalContent">
+          <div className="modal-content">
+            <h2>{title}</h2>
             <span className="close" onClick={closeModal}>
-              &times;
+              {exittext}
             </span>
-            <h2>비밀번호</h2>
-            <input
-              type="password"
-              placeholder="비밀번호 입력"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <button onClick={handleSubmit}>수정하러 가기</button>
+            <div>{content}</div>
+            <button className="submit-button" onClick={handleButtonClick}>{buttontext}</button>
           </div>
         </div>
       )}
