97876af97665a0e141ba1dec39e9cc86e2608f77
mingmungXD <rex02033@gmail.com>
2024-10-06 17:19:23 +0900

    19 feat 모달 추가 (#24)
    * feat:모달 js파일 추가
    
    * feat: 공통 모달 추가
    
    * 모달 페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HabitPage from "./pages/HabitPage";
-import TestPage from "./pages/testPage.js";
+import Test from "./pages/test";
+import Modal from "./components/commons/modal/Modal";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/habit" element={<HabitPage />} />
-        <Route path="/" element={<TestPage />} />
+        <Route path="/" element={<Test />} />
+        <Route path="/modal" element={<Modal />} />
       </Routes>
     </Router>
   );
