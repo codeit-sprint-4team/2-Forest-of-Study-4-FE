3114f3424f09b27aeb72e242cae27b9571c026b5
alscksdlek <mseon916@naver.com>
2024-10-07 09:54:53 +0900

    style: main에서 디자인 충돌 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Test from "./pages/test";
+import Test from "./pages/testPage";
 import Modal from "./components/commons/modal/Modal";
-
+import HabitPage from "./pages/HabitPage";
 export default function App() {
   return (
     <Router>
-        <Route path="/habits" element={<HabitPage />} 
+      <Routes>
+        <Route path="/habits" element={<HabitPage />} />
         <Route path="/" element={<Test />} />
         <Route path="/modal" element={<Modal />} />
       </Routes>
