bb21adbcbb3cd36f47c5e2576e02fdf5b775fdfa
alscksdlek <mseon916@naver.com>
2024-10-07 18:22:06 +0900

    style: padding 수정

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDom from "react-dom/client";
 import App from "./App";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<App />, root);
+root.render(<App />);
