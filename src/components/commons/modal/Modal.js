4e6d54481f690d4a024beb9c6a925a6a878b45b1
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2024-10-07 20:05:07 +0900

    feat: habit에 공통 컴포넌트 모달 추가 및 모달 스타일 수정 (#41)
    * style: 모달 스타일 수정
    
    * style: 폰트 추가
    
    * feat: 예시 모달 삭제 공통 모달 추가
    
    * feat: 모달 버튼 삭제
    
    * feat: habitPage 모달 내용 컴포넌트 생성 및 로직 추가
    
    * style: padding 수정
    
    * style: border-radius 추가
    
    * style: 줄바꿈 제거
    
    ---------
    
    Co-authored-by: alscksdlek <mseon916@naver.com>

--- src/components/commons/modal/Modal.js
+++ src/components/commons/modal/Modal.js
@@ -1,34 +1,40 @@
-import React, { useState } from "react";
-import '../../../style/Modal.css'
-import { Link } from "react-router-dom";
-import btn_habit from "../../../assets/imgs/btn_habit.png"
-import btn_focus from "../../../assets/imgs/btn_focus.png"
+import React from "react";
+import "../../../style/Modal.css";
+import { Link, useLocation } from "react-router-dom";
+import btn_habit from "../../../assets/imgs/btn_habit.png";
+import btn_focus from "../../../assets/imgs/btn_focus.png";
 
-function Modal({ title, exittext, content, buttontext, handleButtonClick}) {
-  const [isOpen, setIsOpen] = useState(false);
+function Modal({
+  title,
+  modalContent,
+  exitText,
+  buttonText,
+  handleButtonClick,
+}) {
+  const location = useLocation();
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => {
-    setIsOpen(false);
-  };
   return (
-    <div>
-      <button onClick={openModal}>모달을 여는 버튼입니다</button>
-      {isOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <h2>{title}</h2>
-            <span className="close" onClick={closeModal}>
-              {exittext}
-            </span>
-            <div>{content}</div>
-            <button className="submit-button" onClick={handleButtonClick}>{buttontext}</button>
-          </div>
-          <Link to="/Habit"> 
-            <img src={btn_habit} />
+    <div className="CommonModal">
+      <div className="modalBox">
+        <h2 className="modalTitle">{title}</h2>
+        <div className="modalContent">{modalContent}</div>
+        <div className="modalButton">
+          <button className="modalClose" onClick={handleButtonClick}>
+            {exitText}
+          </button>
+          <button className="submitButton" onClick={handleButtonClick}>
+            {buttonText}
+          </button>
+        </div>
+      </div>
+      {(location.pathname === "/study-detail" ||
+        location.pathname === "/modal") && (
+        <div className="movePage">
+          <Link to="/Habit">
+            <img src={btn_habit} alt="btn_habit" className="modalImg" />
           </Link>
           <Link to="/Focus">
-            <img src={btn_focus} />
+            <img src={btn_focus} alt="btn_focus" className="modalImg" />
           </Link>
         </div>
       )}
