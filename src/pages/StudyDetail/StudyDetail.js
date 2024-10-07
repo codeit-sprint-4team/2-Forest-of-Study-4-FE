f32570572bd013c37842e7433bf2d56104b3d948
mingmungXD <rex02033@gmail.com>
2024-10-07 14:54:41 +0900

    스터디상세페이지

---
+++ src/pages/StudyDetail/StudyDetail.js
@@ -0,0 +1,30 @@
+import Header from "../../components/commons/header/Header";
+import HabitTable from "./Habittable.js";
+import Introduce from "./Introduce.js";
+import "../../style/StudyDetail.css";
+import Gnb from "../../components/commons/gnb/Gnb.js"
+
+const StudyPage = () => {
+  return (
+    <>
+      <Gnb/>
+      <div className="study">
+        <div className="study-container">
+          <Header
+            title="연우"
+            buttonTo1="/"
+            buttonTo2="/"
+            buttonTitle1="오늘의 습관"
+            buttonTitle2="오늘의 집중"
+          />
+          <Introduce 
+            content="오늘 하루도 화이팅 :)"
+            point="310"
+          />
+          <HabitTable />
+        </div>
+      </div>
+    </>
+  );
+};
+export default StudyPage;
