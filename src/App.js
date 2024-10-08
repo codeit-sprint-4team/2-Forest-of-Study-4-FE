900555b6d7a11c88cbbb6e7eecb858dbfe301368
rlackgus <39423417+rlackgus@users.noreply.github.com>
2024-10-08 18:10:02 +0900

    13 feat 스터디 리스트 페이지 (#46)
    * feat:스터디 리스트 페이지 수정
    
    * feat:반응형 추가 및 css 수정
    
    * feat: 최근 조회한 스터디 기능 추가 및 css 수정
    
    * feat : GNB 스터디만들기 기능추가

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import HabitPage from "./pages/HabitPage";
 import TestPage from "./pages/testPage.js";
 import StudyPage from "./pages/StudyPage";
 import StudyDetail from "./pages/StudyDetail/StudyDetail.js";
+import StudyListPage from "./pages/StudyListPage"; //스터디 리스트 페이지
+import CreateStudyPage from "./pages/CreateStudyPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/createStudy" element={<CreateStudyPage />} />
+        <Route path="/habit" element={<HabitPage />} />
         <Route path="/study-check" element={<StudyPage />} />
         <Route path="/habits" element={<HabitPage />} />
         <Route path="/test" element={<TestPage />} />
         <Route path="/modal" element={<Modal />} />
         <Route path="/study-detail" element={<StudyDetail />} />
+        <Route path="/" element={<StudyListPage />} />
       </Routes>
     </Router>
   );
